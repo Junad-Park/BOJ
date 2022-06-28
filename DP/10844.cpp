f861ae5118909e8616e83155a1d899ad85691796
Park Junha <qkrwnsgk1624@naver.com>
2022-06-29 00:32:08 +0900

    6/29

--- DP/10844.cpp
+++ DP/10844.cpp
@@ -2,16 +2,44 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
-int dp[101];
-
+long dp[101][10];
+long mod = 1000000000;
 int stairs_Num(int N)
 {
-  dp[1] = 9;
-  for (int i = 2; i <= N; i++)
+  long result = 0;
+
+  for (int i = 1; i < 10; i++)
   {
-    dp[i] = (2 * dp[i - 1] - 1) % 1000000000;
+    dp[1][i] = 1;
   }
-  return dp[N];
+
+  for (int i = 2; i <= 100; i++)
+  {
+
+    for (int j = 0; j < 10; j++)
+    {
+      if (j == 0)
+      {
+        dp[i][j] = (dp[i - 1][j + 1]) % mod;
+      }
+      else if (j == 9)
+      {
+        dp[i][j] = (dp[i - 1][j - 1]) % mod;
+      }
+      else
+      {
+        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+      }
+      // cout << "dp[" << i << "][" << j << "] : " << dp[i][j] << endl;
+    }
+  }
+
+  for (int i = 0; i < 10; i++)
+  {
+    result += dp[N][i];
+  }
+
+  return result % mod;
 }
 
 int main()
